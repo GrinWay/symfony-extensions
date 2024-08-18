40aaa0035227e922b7923292728e99b41294a3f3
Grin <son5-29@mail.ru>
2024-08-18 21:18:38 +1200

    fix(php code beautifier)

--- src/Configurator/GlobalInstanceOfConfigurator.php
+++ src/Configurator/GlobalInstanceOfConfigurator.php
@@ -6,25 +6,25 @@ use GrinWay\Extension\Config\GlobalInstanceOfDefaults;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 
 return static function (DefinitionConfigurator $definition) {
-	return $definition->rootNode()
-		->children()
-			->arrayNode(GlobalInstanceOfDefaults::PREFIX)
-				->canBeDisabled()
-				->addDefaultsIfNotSet()
-				->children()
-			
-					->scalarNode(GlobalInstanceOfDefaults::REL_PATH_CONFIG_KEY)
-						->info('The relative path to directory where file with _instanceof locates to assign tags globally')
-						->defaultValue(GlobalInstanceOfDefaults::REL_PATH)
-					->end()
+    return $definition->rootNode()
+        ->children()
+            ->arrayNode(GlobalInstanceOfDefaults::PREFIX)
+                ->canBeDisabled()
+                ->addDefaultsIfNotSet()
+                ->children()
 
-					->scalarNode(GlobalInstanceOfDefaults::FILENAME_CONFIG_KEY)
-						->info('The filename with _instanceof content.')
-						->defaultValue(GlobalInstanceOfDefaults::FILENAME)
-					->end()
+                    ->scalarNode(GlobalInstanceOfDefaults::REL_PATH_CONFIG_KEY)
+                        ->info('The relative path to directory where file with _instanceof locates to assign tags globally')
+                        ->defaultValue(GlobalInstanceOfDefaults::REL_PATH)
+                    ->end()
 
-				->end()
-			->end()
-		->end()
-	;
+                    ->scalarNode(GlobalInstanceOfDefaults::FILENAME_CONFIG_KEY)
+                        ->info('The filename with _instanceof content.')
+                        ->defaultValue(GlobalInstanceOfDefaults::FILENAME)
+                    ->end()
+
+                ->end()
+            ->end()
+        ->end()
+    ;
 };
